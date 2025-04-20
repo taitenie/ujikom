b62f58836e18a5ee5e14ea51eea5cc52f5087c1f
UrayMR <umrajsyafiq@gmail.com>
2025-04-20 19:45:34 +0700

    feat: add modal for shop creation

---
+++ resources/views/components/modal-shopCreation.blade.php
@@ -0,0 +1,17 @@
+<div class="modal fade" id="shopCreationModal" data-bs-backdrop=static data-bs-backdrop="static" data-bs-keyboard="false" aria-labelledby="shopCreationLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="shopCreationLabel">Confirmation</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <p>Do you really want to request shop creation?</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
+        <button type="button" class="btn btn-primary">Yes</button>
+      </div>
+    </div>
+  </div>
+</div>
